e0badd8ac61308ecedcac9bc2c71bcb325f56404
ederson lopez <ederson12f@gmail.com>
2021-12-10 17:00:42 -0500

    get the categories from the API

--- src/components/ListOfCategories/index.jsx
+++ src/components/ListOfCategories/index.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import { categories } from '../../db'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 
 import { List, Item } from './styles'
 
 const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+  useEffect(() => {
+    window.fetch('https://edexgram-api-edex.vercel.app/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
   return (
     <List>
       {
