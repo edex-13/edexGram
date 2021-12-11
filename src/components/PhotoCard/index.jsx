6aa9be456afd6086f50a1898d52ef040b8e89b4d
ederson lopez <ederson12f@gmail.com>
2021-12-11 16:19:46 -0500

    lazy loading in PhotoCard

--- src/components/PhotoCard/index.jsx
+++ src/components/PhotoCard/index.jsx
@@ -1,22 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
-import { ImgWrapper, Img, Button } from './styles'
+import { Article, ImgWrapper, Img, Button } from './styles'
 import { MdFavoriteBorder } from 'react-icons/md'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  const element = useRef(null)
+  const [show, setShow] = useState(false)
+  useEffect(() => {
+    const observer = new window.IntersectionObserver(entries => {
+      const { isIntersecting } = entries[0]
+      if (isIntersecting) {
+        setShow(true)
+        console.log('si')
+        observer.disconnect()
+      }
+    })
+    observer.observe(element.current)
+  }, [element])
   return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} />
-        </ImgWrapper>
-      </a>
-      <Button>
-        <MdFavoriteBorder size='25px' />{likes} likes!
-      </Button>
-    </article>
+    <Article ref={element}>
+      {
+        show &&
+          <>
+            <a href={`/detail/${id}`}>
+              <ImgWrapper>
+                <Img src={src} />
+              </ImgWrapper>
+            </a>
+            <Button>
+              <MdFavoriteBorder size='25px' />{likes} likes!
+            </Button>
+          </>
+      }
+
+    </Article>
   )
 }
 
--- src/components/PhotoCard/styles.js
+++ src/components/PhotoCard/styles.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components'
 
 import { fadeInd } from '../../styles/animation'
+
+export const Article = styled.article`
+  min-height:200px;
+`
 export const ImgWrapper = styled.div`
   border-radius: 10px;
   display: block;
